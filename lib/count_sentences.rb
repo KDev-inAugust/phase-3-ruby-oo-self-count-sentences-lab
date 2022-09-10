1a8f64f3f6167168f610b032a3827d0875ab9fa4
Kane Mathis <admin@kanemathis.com>
2022-09-10 13:58:10 -0400

    commit tree 3e2146f247c56d41ea1877600d9925f48ea35560

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1]=="."
+     true
+    else
+     false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
   end
+end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    count_arr=self.split(/[.!?]/)
+    check_arr=[]
+    count_arr.map {|index| if index.match(/[a-z]/)
+    check_arr << index 
+  end}
+  check_arr.length
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
